e8cf080e3520259109fe3c0d9773d077d1d41cfc
harrisb002 <harrisbe@sonoma.edu>
2024-09-06 23:40:07 -0700

    wrapping up

--- Scripts/Hud.gd
+++ Scripts/Hud.gd
@@ -1,37 +1,43 @@
 extends CanvasLayer
 
-# Use signal to notify 'Main' that the button has been pressed
+# Signal to notify 'Main' that the button has been pressed
 signal start_game
-	
-func show_message(text):
-	$Message.text = text
-	$Message.show()
-	$MessageTimer.start()
-	
-func show_game_over():
-	show_message("Game Over")
-	
-	#Wait until the message timer has timed out
+
+# Called when the scene is ready
+func _ready() -> void:
+	# Show the initial message "Dodge the Creeps!" and the start button
+	show_message("Dodge the Creeps!")
+	$StartButton.show()  # Display the start button at the beginning
+
+# Function to display messages like "Game Over" or "Dodge the Creeps!"
+func show_message(text: String) -> void:
+	$Message.text = text  # Set the text for the message label
+	$Message.show()  # Show the message
+	$MessageTimer.start()  # Start a timer to hide the message after a short delay
+
+# Function to show the "Game Over" message, followed by "Dodge the Creeps!"
+func show_game_over() -> void:
+	show_message("Game Over")  # Show the game over message
+
+	# Wait for the message timer to time out
 	await $MessageTimer.timeout
 	
+	# Change the message to "Dodge the Creeps!" and display it
 	$Message.text = "Dodge the Creeps!"
 	$Message.show()
-	
-	# Make a one shot timer and wait for it to finish
-	# Await for the scene tree to finish
-	await get_tree().create_timer(1.0).timeout
-	
-	$StartButton.show()
-	
-func update_score(score):
-	$ScoreLabel.text = str(score)
-	
-	
-# Connecting to the HUD node from the StartButton
-func _on_start_button_pressed() -> void:
-	$StartButton.hide()
-	start_game.emit()
 
-# Connecting to the HUD node from the MessageTimer
-func _on_message_timer_timeout() -> void:
-	pass # Replace with function body.
+	# Create a one-shot timer to wait for 1 second before showing the Start button
+	await get_tree().create_timer(1.0).timeout
+
+	# Show the Start button again for the player to restart the game
+	$StartButton.show()
+
+# Update the score on the HUD
+func update_score(score: int) -> void:
+	$ScoreLabel.text = str(score)  # Convert the score to a string and update the score label
+
+# Handle the Start button press event
+func _on_start_button_pressed() -> void:
+	$StartButton.hide()  # Hide the start button when pressed
+	$Message.hide()  # Hide any messages when the game starts
+	start_game.emit()  # Emit the signal to notify 'Main' that the game has started
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,66 +1,71 @@
 extends Node
 
-@export var mob_scene: PackedScene
-var score
+@export var mob_scene: PackedScene  # The mob scene to instantiate during the game.
+var score  # The player's score.
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
+	pass  
 
+# This function is called when the player dies or the game ends.
 func game_over() -> void:
-	$ScoreTimer.stop()
-	$MobTimer.stop()
-	$HUD.show_game_over()
-	$Music.stop()
-	$DeathSound.play()
+	$ScoreTimer.stop()  # Stop the score timer since the game is over.
+	$MobTimer.stop()  # Stop spawning new mobs.
+	$HUD.show_game_over()  # Show the game over message.
+	$Music.stop()  # Stop the background music.
+	$DeathSound.play()  # Play the death sound when the player dies.
 
-func new_game():
-	score = 0
-	$Player.start($StartPosition.position)
-	$StartTimer.start()
+# This function starts a new game.
+func new_game() -> void:
+	$Music.play()  # Start playing the background music at the beginning of the game.
 	
-	# Update the score display and show the "Get Ready" message
+	score = 0  # Reset the score to 0.
+	$Player.start($StartPosition.position)  # Move the player to the start position.
+	
+	# Update the score display and show the "Get Ready" message on the HUD.
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
-	
-	# Get the mobs group so remove all mobs from the screen collectively
+
+	# Clear all mobs from the screen by calling `queue_free()` on all objects in the "mobs" group.
 	get_tree().call_group("mobs", "queue_free")
 	
-	$Music.play()
-	
-func _on_start_timer_timeout() -> void:
-	$MobTimer.start()
-	$ScoreTimer.start()
+	# Delay the start of the game by waiting for a few seconds before starting the mobs and score.
+	# Will wait for 2 seconds before starting the creeps and the score timer.
+	await get_tree().create_timer(2.0).timeout  # Wait for 2 seconds.
+	$HUD.show_message("")
 
+	$MobTimer.start()  # Start spawning mobs at regular intervals.
+	$ScoreTimer.start()  # Start the score timer to increment the score periodically.
 
+# Called each time the score timer times out.
 func _on_score_timer_timeout() -> void:
-	score += 1
+	score += 1  # Increment the score by 1.
 	
-	# Keep the display in sync with the changing score
+	# Update the score display on the HUD to reflect the new score.
 	$HUD.update_score(score)
 
-# When this times out it will process this
+# Called each time the mob timer times out to spawn a new mob.
 func _on_mob_timer_timeout() -> void:
-	# Create a Mob instance and add it to the scene.
+	# Create a new instance of the mob scene and add it to the main scene.
 	var mob = mob_scene.instantiate()
 	
-	# Choose a random location on Path2D.
+	# Choose a random location along the Path2D to spawn the mob.
 	var mob_spawn_location = $MobPath/MobSpawnLocation
-	mob_spawn_location.progress_ratio = randf()
+	mob_spawn_location.progress_ratio = randf()  # Set the spawn location randomly along the path.
 	
 	# Set the mob's direction perpendicular to the path direction.
 	var direction = mob_spawn_location.rotation + PI / 2
 
-	# Set the mob's position to a random location.
+	# Set the mob's position to the chosen random location.
 	mob.position = mob_spawn_location.position
 
-	# Add some randomness to the direction.
+	# Add some randomness to the direction in which the mob will move.
 	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
+	mob.rotation = direction  # Rotate the mob to face the chosen direction.
 
-	# Choose the velocity (speed & direction) for the mob.
+	# Choose the mob's speed and direction.
 	var velocity = Vector2(randf_range(150.0, 250.0), 0)
-	mob.linear_velocity = velocity.rotated(direction)
+	mob.linear_velocity = velocity.rotated(direction)  # Apply the velocity with the rotated direction.
 	
-	# Spawn the mob by adding it to the main scene
+	# Add the mob to the scene so it can appear and move.
 	add_child(mob)
